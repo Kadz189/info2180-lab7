d18a2fae4f07d63ebb1aa52665317560ba732602
Kadz189 <32443312+Kadz189@users.noreply.github.com>
2017-11-17 13:04:00 -0500

    Add files via upload

--- world.php
+++ world.php
@@ -5,14 +5,27 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries");
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$country = $_GET['country'];
+$All = $_GET['all'];
+
+//print_r($_GET);
+
+if ($All == "true"){
+    $stmt = $conn->query("SELECT * from countries");
+} else {
+    $stmt = $conn->query("SELECT * from countries where name = '$country'");
+}
+
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
 echo '<ul>';
 foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li><br>';
 }
 echo '</ul>';
